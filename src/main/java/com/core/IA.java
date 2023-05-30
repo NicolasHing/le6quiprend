07d878186ecaaf1075fdabdd5f8d8c2955ded556
Amine1102 <amine.laimeche@gmail.com>
2023-05-30 18:04:39 +0200

    IA a modifier

--- src/main/java/com/core/IA.java
+++ src/main/java/com/core/IA.java
@@ -1,8 +1,18 @@
 package com.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IA extends AbstractJoueur{
     public IA() {
         super("IA");
+    }
+
+    @Override
+    public List<Carte> choisirCartesAJouer(Plateau plateau){
+        List<Carte> cartesAJouer = new ArrayList<>();
+
+        //L'ia choisi une carte au hasard
     }
 
 }
