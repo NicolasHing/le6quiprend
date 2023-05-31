722b0567c6b175b9c5dcbdf21eef8af341d6dd35
Leonicolash <nicolas.hing@gmail.com>
2023-05-31 18:00:02 +0200

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,8 @@ module com.example.le6quiprend {
 
     opens com.example.le6quiprend to javafx.fxml;
     exports com.example.le6quiprend;
-    exports com.core;
-    opens com.core to javafx.fxml;
+    exports com.example.le6quiprend.core;
+    opens com.example.le6quiprend.core to javafx.fxml;
+    exports com.example.le6quiprend.view;
+    opens com.example.le6quiprend.view to javafx.fxml;
 }
